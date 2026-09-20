6f0b8ac73d1815b1e2c7852a5d738f084497b928
Nazar <kakentai804@gmail.com>
2026-09-20 17:39:43 +0500

    Implement Factory Method pattern

---
+++ src/main/java/factorymethod/Logistics.java
@@ -0,0 +1,11 @@
+package factorymethod;
+
+public abstract class Logistics {
+
+    public void planDelivery() {
+        Transport transport = createTransport();
+        transport.deliver();
+    }
+
+    protected abstract Transport createTransport();
+}
---
+++ src/main/java/factorymethod/Ship.java
@@ -0,0 +1,9 @@
+package factorymethod;
+
+public class Ship implements Transport {
+
+    @Override
+    public void deliver() {
+        System.out.println("Delivering by ship.");
+    }
+}
